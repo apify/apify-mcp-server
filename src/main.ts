a6d3e1cad052addf9e66d57bfb1023ec29c2f2f4
Jiri Spilka <jirka.spilka@gmail.com>
2025-03-21 14:29:50 +0100

    fix: remove double envelope data

--- src/main.ts
+++ src/main.ts
@@ -34,28 +34,26 @@ const HELP_MESSAGE = `Connect to the server with GET request to ${HOST}/sse?toke
     + ` and then send POST requests to ${HOST}/message?token=YOUR-APIFY-TOKEN`;
 
 const actorRun = Actor.isAtHome() ? {
-    data: {
-        id: process.env.ACTOR_RUN_ID,
-        actId: process.env.ACTOR_ID,
-        userId: process.env.APIFY_USER_ID,
-        startedAt: process.env.ACTOR_STARTED_AT,
-        finishedAt: null,
-        status: 'RUNNING',
-        meta: {
-            origin: process.env.APIFY_META_ORIGIN,
-        },
-        options: {
-            build: process.env.ACTOR_BUILD_NUMBER,
-            memoryMbytes: process.env.ACTOR_MEMORY_MBYTES,
-        },
-        buildId: process.env.ACTOR_BUILD_ID,
-        defaultKeyValueStoreId: process.env.ACTOR_DEFAULT_KEY_VALUE_STORE_ID,
-        defaultDatasetId: process.env.ACTOR_DEFAULT_DATASET_ID,
-        defaultRequestQueueId: process.env.ACTOR_DEFAULT_REQUEST_QUEUE_ID,
-        buildNumber: process.env.ACTOR_BUILD_NUMBER,
-        containerUrl: process.env.ACTOR_WEB_SERVER_URL,
-        standbyUrl: process.env.ACTOR_STANDBY_URL,
+    id: process.env.ACTOR_RUN_ID,
+    actId: process.env.ACTOR_ID,
+    userId: process.env.APIFY_USER_ID,
+    startedAt: process.env.ACTOR_STARTED_AT,
+    finishedAt: null,
+    status: 'RUNNING',
+    meta: {
+        origin: process.env.APIFY_META_ORIGIN,
     },
+    options: {
+        build: process.env.ACTOR_BUILD_NUMBER,
+        memoryMbytes: process.env.ACTOR_MEMORY_MBYTES,
+    },
+    buildId: process.env.ACTOR_BUILD_ID,
+    defaultKeyValueStoreId: process.env.ACTOR_DEFAULT_KEY_VALUE_STORE_ID,
+    defaultDatasetId: process.env.ACTOR_DEFAULT_DATASET_ID,
+    defaultRequestQueueId: process.env.ACTOR_DEFAULT_REQUEST_QUEUE_ID,
+    buildNumber: process.env.ACTOR_BUILD_NUMBER,
+    containerUrl: process.env.ACTOR_WEB_SERVER_URL,
+    standbyUrl: process.env.ACTOR_STANDBY_URL,
 } : {};
 
 /**
