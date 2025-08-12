900477f103a9b0a98f0ed10622aaefd8e40ea0da
Michal Kalita <kalita.michal@gmail.com>
2025-08-12 14:24:07 +0200

    fix: code review improvements

--- src/tools/utils.ts
+++ src/tools/utils.ts
@@ -298,7 +298,8 @@ export function separateAdvancedInputsInSchema(properties: Record<string, ISchem
     propObject.advancedInput = {
         type: 'object',
         title: 'Advanced Inputs',
-        description: 'These inputs are considered advanced and are not required for basic functionality.',
+        description: 'Universal object for advanced inputs. Full definition is available in tool get-actor-detail. '
+        + 'These inputs are considered advanced and are not required for basic functionality.',
         properties: Object.fromEntries(advancedInputs),
     } as unknown as ISchemaProperties;
 
