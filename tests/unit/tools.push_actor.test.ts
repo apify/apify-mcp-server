74fe9b1a1b9d40018094fa575b10f83cc38ca9aa
David Hanuš <dh.david.hanus@gmail.com>
2026-09-15 12:04:43 +0200

    test: Import ACTOR_CONFIG_PATH in the push-actor boundary tests

--- tests/unit/tools.push_actor.test.ts
+++ tests/unit/tools.push_actor.test.ts
@@ -7,6 +7,7 @@ import { MAX_MULTIFILE_BYTES } from '@apify/consts';
 import { FAILURE_CATEGORY, HELPER_TOOLS, TOOL_STATUS } from '../../src/const.js';
 import { WAIT_SECS_MAX } from '../../src/tools/actors/actor_run_response.js';
 import { pushActor } from '../../src/tools/deploy/push_actor.js';
+import { ACTOR_CONFIG_PATH } from '../../src/tools/deploy/source_files.js';
 import { pushActorToolOutputSchema } from '../../src/tools/structured_output_schemas.js';
 import type { HelperTool, InternalToolArgs } from '../../src/types.js';
 import { VERBATIM_LINKS_NUDGE } from '../../src/utils/console_link.js';
